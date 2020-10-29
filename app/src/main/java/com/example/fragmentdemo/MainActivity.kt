0774043b7f30a614840d1fd8368aeb7f495e8cee
LAU KA WAI <17602055563@163.com>
2020-10-29 19:55:44 +0800

    项目初始化：实现平板动画效果

---
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.fragmentdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.left_fragment.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener{
+            replaceFragment(AnotherRightFragment())
+        }
+        replaceFragment(RightFragment())
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val transaction =supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout,fragment)
+        transaction.commit()
+    }
+}
